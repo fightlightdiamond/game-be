5fe59d7cef835a48806e95d649ca4bbd2b2209f2
cuong.pham <i.am.m.cuong@gmail.com>
2022-07-19 21:04:47 +0700

    Mysql Setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfigAsync } from './config/typeorm.config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    // Mysql
+    TypeOrmModule.forRootAsync(typeormConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,32 @@
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export default class TypeormConfig {
+  static getOrmConfig(config: ConfigService): TypeOrmModuleOptions {
+    console.log(config.get<string>('DB_USERNAME'))
+    return {
+      type: 'mysql',
+      host: config.get<string>('DB_HOST'),
+      port: config.get<number>('DB_PORT'),
+      username: config.get<string>('DB_USERNAME'),
+      password: config.get<string>('DB_PASSWORD'),
+      database: config.get<string>('DB_DATABASE'),
+      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      synchronize: config.get<string>('DB_SYNCHRONIZE') === 'true',
+      logging: true,
+      extra: {
+        connectionLimit: 50,
+      },
+    };
+  }
+}
+
+export const typeormConfigAsync: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService): TypeOrmModuleOptions =>
+    TypeormConfig.getOrmConfig(config),
+};
