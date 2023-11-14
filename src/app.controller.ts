549c6767c3688eef843654c9cd5495b3a4c61bde
FightLightDiamond <i.am.m.cuong@gmail.com>
2023-11-14 22:28:55 +0700

    svg out put
    svg out put

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 
-const mjAPI = require('mathjax-node');
+import * as mjAPI from 'mathjax-node';
+import * as mjpage from 'mathjax-node-page';
 
 mjAPI.config({
   MathJax: {
@@ -18,9 +19,7 @@ export class AppController {
 
   @Get('mathjax')
   async mathjax() {
-    // var yourMath = 'E = mc^2';
-    const yourMath =
-      '$$\\large \\begin{array}{r} \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ①}}1\\textcolor[gray]{0}{^{\\scriptsize ⑤}}1\\textcolor[gray]{0}{^{\\scriptsize ⑨}}1} \\hspace{0.5pt}\\\\[-3pt] 1\\phantom{^{\\scriptsize ①}}1\\enclose{longdiv}{\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ②}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}0^{\\scriptsize ①}0}}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ③}}1\\textcolor[gray]{0}{^{\\scriptsize ④}}1\\phantom{^{\\scriptsize ①}0}} \\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0} \\textcolor[gray]{0}{^{\\scriptsize ⑥}}1\\phantom{^{\\scriptsize ③}}1\\phantom{^{\\scriptsize ③}0}}} \\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑦}}1\\textcolor[gray]{0}{^{\\scriptsize ⑧}}1}\\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0}\\phantom{^{\\scriptsize ①}0}\\textcolor[gray]{0}{^{\\scriptsize ⑩}}1\\phantom{^{\\scriptsize ⑧}}1}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑪}}1\\phantom{^{\\scriptsize ⑪}}1}\\hspace{0.5pt}\\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\end{array} \\hspace{1.0cm} \\begin{array}{l} {\\tiny ①たてる　11\\div11} \\phantom{0}\\\\[-10pt] {\\tiny \\phantom{①たてる　} \\ ⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ②かける　11\\times1=11}\\\\[-10pt] {\\tiny ③ひく　　11-11=1}\\\\[-10pt] {\\tiny ④おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑤たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑤たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑥かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑦ひく　　11-11=1} \\\\[-10pt] {\\tiny ⑧おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑨たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑨たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑩かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑪ひく　　11-11=11（あまり）} \\end{array} \\large \\begin{array}{r} \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ①}}1\\textcolor[gray]{0}{^{\\scriptsize ⑤}}1\\textcolor[gray]{0}{^{\\scriptsize ⑨}}1} \\hspace{0.5pt}\\\\[-3pt] 1\\phantom{^{\\scriptsize ①}}1\\enclose{longdiv}{\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ②}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}0^{\\scriptsize ①}0}}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ③}}1\\textcolor[gray]{0}{^{\\scriptsize ④}}1\\phantom{^{\\scriptsize ①}0}} \\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0} \\textcolor[gray]{0}{^{\\scriptsize ⑥}}1\\phantom{^{\\scriptsize ③}}1\\phantom{^{\\scriptsize ③}0}}} \\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑦}}1\\textcolor[gray]{0}{^{\\scriptsize ⑧}}1}\\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0}\\phantom{^{\\scriptsize ①}0}\\textcolor[gray]{0}{^{\\scriptsize ⑩}}1\\phantom{^{\\scriptsize ⑧}}1}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑪}}1\\phantom{^{\\scriptsize ⑪}}1}\\hspace{0.5pt}\\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\end{array} \\hspace{1.0cm} \\begin{array}{l} {\\tiny ①たてる　11\\div11} \\phantom{0}\\\\[-10pt] {\\tiny \\phantom{①たてる　} \\ ⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ②かける　11\\times1=11}\\\\[-10pt] {\\tiny ③ひく　　11-11=1}\\\\[-10pt] {\\tiny ④おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑤たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑤たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑥かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑦ひく　　11-11=1} \\\\[-10pt] {\\tiny ⑧おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑨たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑨たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑩かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑪ひく　　11-11=11（あまり）} \\end{array} \\large \\begin{array}{r} \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ①}}1\\textcolor[gray]{0}{^{\\scriptsize ⑤}}1\\textcolor[gray]{0}{^{\\scriptsize ⑨}}1} \\hspace{0.5pt}\\\\[-3pt] 1\\phantom{^{\\scriptsize ①}}1\\enclose{longdiv}{\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}}1}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ②}}1\\phantom{^{\\scriptsize ①}}1\\phantom{^{\\scriptsize ①}0^{\\scriptsize ①}0}}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ③}}1\\textcolor[gray]{0}{^{\\scriptsize ④}}1\\phantom{^{\\scriptsize ①}0}} \\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0} \\textcolor[gray]{0}{^{\\scriptsize ⑥}}1\\phantom{^{\\scriptsize ③}}1\\phantom{^{\\scriptsize ③}0}}} \\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑦}}1\\textcolor[gray]{0}{^{\\scriptsize ⑧}}1}\\hspace{0.5pt}\\\\[-3pt] \\underline{\\textcolor[gray]{0.8}{\\phantom{^{\\scriptsize ①}0}\\phantom{^{\\scriptsize ①}0}\\textcolor[gray]{0}{^{\\scriptsize ⑩}}1\\phantom{^{\\scriptsize ⑧}}1}}\\hspace{0.5pt}\\\\[-3pt] \\textcolor[gray]{0.8}{\\textcolor[gray]{0}{^{\\scriptsize ⑪}}1\\phantom{^{\\scriptsize ⑪}}1}\\hspace{0.5pt}\\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\\\[-3pt] \\end{array} \\hspace{1.0cm} \\begin{array}{l} {\\tiny ①たてる　11\\div11} \\phantom{0}\\\\[-10pt] {\\tiny \\phantom{①たてる　} \\ ⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ②かける　11\\times1=11}\\\\[-10pt] {\\tiny ③ひく　　11-11=1}\\\\[-10pt] {\\tiny ④おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑤たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑤たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑥かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑦ひく　　11-11=1} \\\\[-10pt] {\\tiny ⑧おろす}\\phantom{0}\\\\[-10pt] {\\tiny ⑨たてる　11\\div11}\\phantom{0}\\\\[-10pt] {\\tiny \\phantom{⑨たてる　}⇒\\ 1}\\phantom{0}\\\\[-10pt] {\\tiny ⑩かける　11\\times1=11}\\\\[-10pt] {\\tiny ⑪ひく　　11-11=11（あまり）} \\end{array}$$';
+    const yourMath = 'E = mc^2';
 
     const result = await mjAPI.typeset({
       math: yourMath,
@@ -29,5 +28,82 @@ export class AppController {
     });
 
     return result.html;
+  }
+
+  @Get('page')
+  async page() {
+    const input = `
+  \\[ f(x) = \\int_{-\\infty}^{\\infty} e^{-\\pi x^2} \\, dx \\]
+  `;
+
+    try {
+      return await this.renderMath(input);
+    } catch (error) {
+      console.error('Math rendering failed:', error);
+    }
+  }
+
+  async renderMath(input) {
+    return new Promise((resolve, reject) => {
+      const mjpageConfig = {
+        format: ['TeX'], // determines type of pre-processors to run
+        output: 'html', // global override for output option; 'svg', 'html' or 'mml'
+        tex: {}, // configuration options for tex pre-processor, cf. lib/tex.js
+        ascii: {}, // configuration options for ascii pre-processor, cf. lib/ascii.js
+        singleDollars: false, // allow single-dollar delimiter for inline TeX
+        fragment: false, // return body.innerHTML instead of full document
+        cssInline: true, // determines whether inline css should be added
+        jsdom: {
+          // NOTE these are not straight jsdom configuration options (cf. below)
+          // NOT used when passing jsdom object to mjpage()
+          runScripts: 'dangerously', // set to "dangerously" to execute scripts from the HTML source
+          virtualConsole: true,
+        },
+        displayMessages: false, // determines whether Message.Set() calls are logged
+        displayErrors: false, // determines whether error messages are shown on the console
+        undefinedCharError: false, // determines whether unknown characters are saved in the error array
+        extensions: '', // a convenience option to add MathJax extensions
+        fontURL:
+          'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/fonts/HTML-CSS', // for webfont urls in the CSS for HTML output
+        MathJax: {}, // options MathJax configuration, see https://docs.mathjax.org
+        // errorHandler: (id, wrapperNode, sourceFormula, sourceFormat, errors) => {...} // function to handle rendering error
+      };
+      const mjnodeConfig = {
+        ex: 6, // ex-size in pixels
+        width: 100, // width of math container (in ex) for linebreaking and tags
+        useFontCache: true, // use <defs> and <use> in svg output?
+        useGlobalCache: false, // use common <defs> for all equations?
+        // state: mjstate, // track global state
+        linebreaks: false, // do linebreaking?
+        equationNumbers: 'none', // or "AMS" or "all"
+        math: '', // the math to typeset
+        html: false, // generate HTML output?
+        css: false, // generate CSS for HTML output?
+        mml: false, // generate mml output?
+        svg: false, // generate svg output?
+        speakText: true, // add spoken annotations to output?
+        timeout: 10 * 1000, // 10 second timeout before restarting MathJax
+      };
+
+      mjpage.addOutput('html', (wrapper, data) => {
+        wrapper.innerHTML = `<div class='ABC'>
+  ${data}
+</div>`;
+      });
+
+      mjpage
+        .mjpage(input, mjpageConfig, mjnodeConfig, function (output) {
+          if (output.errors) {
+            reject(output.errors);
+          } else {
+            resolve(output);
+          }
+        })
+        .on('afterConversion', function (parsedFormula) {
+          // manipulate parsed result and DOM at your will
+          // see description of parsedFormula object above
+          console.log('parsedFormula', parsedFormula);
+        });
+    });
   }
 }
